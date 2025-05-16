0a0c771667b150405880c64997e0471a60fb8724
Heber Lazo <heberlazo@gmail.com>
2025-05-15 20:11:38 -0500

    corrected

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.get("/", (_, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
